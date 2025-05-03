bcd99aef9271d278bc09b6bed402d3f88ce06ff7
bradyjohnson-tech <upsides.acclaim.0k@icloud.com>
2025-05-03 14:04:23 -0400

    had nested MainActivity class. That was a no bueno, copy and paste QA disaster.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -33,19 +33,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        class MainActivity : ComponentActivity() {
-            override fun onCreate(savedInstanceState: Bundle?) {
-                super.onCreate(savedInstanceState)
-                enableEdgeToEdge()
-                setContent {
-                    DiceRollerTheme {
-                        DiceRollerApp()
-                    }
-                }
+        setContent {
+            DiceRollerTheme {
+                DiceRollerApp()
             }
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
